4ce18c7a2a6f920bb86d7c2e3c622f43f6e40055
Mauritz Marais <mauritz303@gmail.com>
2023-03-05 16:59:21 +0200

    added species detail view and specific species controller function

--- controllers/speciesController.js
+++ controllers/speciesController.js
@@ -1,8 +1,10 @@
 const Species = require("../models/species");
 const Families = require("../models/family");
+const SpeciesInstances = require("../models/speciesInstance");
 
 const async = require("async");
 
+// Display homepage
 exports.index = (req, res) => {
     async.parallel({
         families_count: function (callback) {
@@ -23,21 +25,39 @@ exports.index = (req, res) => {
 
 // Display list of all species.
 exports.species_list = (req, res, next) => {
-    Species.find({}, "common_name")
+    Species.find()
         .exec(function (err, results) {
             if (err) {
                 return next(err);
             }
-            //Successful, so render
-            res.render("species_list", { title: "Species List", species_list: results });
-        });
+            res.render("species_list", { title: "Species List", species_list: results })
+        })
 };
 
-
 // Display detail page for a specific species.
-exports.species_detail = (req, res) => {
-    Species.findById(req.params.id)
-    res.send(`NOT IMPLEMENTED: Species detail: ${req.params.id}`);
+exports.species_detail = (req, res, next) => {
+    async.parallel({
+        species: function (callback) {
+            Species.findById(req.params.id)
+                .populate("family")
+                .populate("genus")
+                .exec(callback)
+        }, species_instances: function (callback) {
+            SpeciesInstances.find({ species: req.params.id }).exec(callback)
+        }
+
+    },
+        (err, results) => {
+            if (err) {
+                return next(err)
+            }
+            res.render("species_detail", {
+                title: `${results.species.common_name} page`,
+                species: results.species,
+                species_instances: results.species_instances
+            });
+        }
+    )
 };
 
 // Display species create form on GET.
