5a10c6a7de50c97030f526b2c9e459767c0103f7
Mauritz Marais <mauritz303@gmail.com>
2023-03-09 17:46:41 +0200

    added form to create new species instance and function to handle post request

--- controllers/speciesinstanceController.js
+++ controllers/speciesinstanceController.js
@@ -1,6 +1,8 @@
 const SpeciesInstance = require("../models/speciesInstance");
 const Species = require("../models/species");
 
+const { body, validationResult } = require("express-validator");
+
 const async = require("async");
 
 // Display list of all SpeciesInstances.
@@ -32,14 +34,67 @@ exports.speciesinstance_detail = (req, res, next) => {
 }
 
 // Display SpeciesInstance create form on GET.
-exports.speciesinstance_create_get = (req, res) => {
-    res.send("NOT IMPLEMENTED: SpeciesInstance create GET");
+exports.speciesinstance_create_get = (req, res, next) => {
+    Species.find().exec(function (err, results) {
+        if (err) {
+            return next(err);
+        }
+        res.render("speciesinstance_form", {
+            title: "Add new animal",
+            species_list: results
+        });
+    });
 };
 
 // Handle SpeciesInstance create on POST.
-exports.speciesinstance_create_post = (req, res) => {
-    res.send("NOT IMPLEMENTED: SpeciesInstance create POST");
-};
+exports.speciesinstance_create_post = [
+    body("animal_name", "Please enter a name")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("animal_age", "Please enter age")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("animal_species", "Please select a species")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        const species_instance = new SpeciesInstance({
+            species: req.body.animal_species,
+            name: req.body.animal_name,
+            age: req.body.animal_age
+        });
+
+        if (!errors.isEmpty()) {
+
+            Species.find().exec(function (err, results) {
+                if (err) {
+                    return next(err);
+                }
+
+                res.render("speciesinstance_form", {
+                    title: "Add new animal",
+                    species_instance,
+                    species_list: results,
+                    errors: errors.array()
+                });
+            });
+            return;
+        }
+
+        species_instance.save(err => {
+            if (err) {
+                return next(err);
+            }
+
+            res.redirect(species_instance.url);
+        });
+    }
+];
 
 // Display SpeciesInstance delete form on GET.
 exports.speciesinstance_delete_get = (req, res) => {
